b3b6e6a21ab2f2170b089f8aacdc15a9ee1231c0
GermanJR <wambows@gmail.com>
2023-03-30 13:42:38 +0200

    Arreglado el comportamiento del pilar de la bateria, implementación completa con la puerta

--- Assets/Scripts/BatterySocketBehaviour.cs
+++ Assets/Scripts/BatterySocketBehaviour.cs
@@ -7,8 +7,15 @@ public class BatterySocketBehaviour : XRSocketInteractor
 {
 
     public GameObject lightSphere;
+    public GameObject canvasError;
+    public GameObject cell;
+    public Animator doorAnimator;
+    public Animator closerAnimator;
 
     private DoorLightBehaviour doorLightBehaviour;
+
+
+    private bool hasCellEntered = false;
 
     // Update is called once per frame
     void Update()
@@ -21,13 +28,32 @@ public class BatterySocketBehaviour : XRSocketInteractor
         if (args.interactableObject.transform.CompareTag("EnergyCell"))
         {
             doorLightBehaviour.ChangeToGreenLight();
+            doorAnimator.SetTrigger("Open");
+            closerAnimator.SetTrigger("Close");
+            StartCoroutine(DisableCellCoroutine());
+            hasCellEntered = true;
+        }
+        else
+        {
+            canvasError.SetActive(true);
         }
         base.OnSelectEntered(args);
     }
 
     protected override void OnSelectExited(SelectExitEventArgs args)
     {
-        doorLightBehaviour.ChangeToRedLight();
+        canvasError.SetActive(false);
+
+        if (!hasCellEntered)
+        {
+            doorLightBehaviour.ChangeToRedLight();
+        }
         base.OnSelectExited(args);
+    }
+
+    private IEnumerator DisableCellCoroutine()
+    {
+        yield return new WaitForSeconds(0.25f);
+        cell.SetActive(false);
     }
 }
