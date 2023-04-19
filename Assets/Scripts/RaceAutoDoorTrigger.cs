645ed62e4261f90ecdf1644ebb0430ac9411e1df
GermanJR <wambows@gmail.com>
2023-04-19 11:36:46 +0200

    Carrera: Añadida la zona de las puertas auto/incorrectas

---
+++ Assets/Scripts/RaceAutoDoorTrigger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceAutoDoorTrigger : MonoBehaviour
+{
+
+    [SerializeField] private Animator doorAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorAnimator.SetTrigger("Open");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorAnimator.SetTrigger("Close");
+        }
+    }
+}
