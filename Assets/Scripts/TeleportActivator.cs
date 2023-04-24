565147749f45a5d11a15cc0e9e62243a556f2449
GermanJR <wambows@gmail.com>
2023-04-24 12:50:54 +0200

    Arreglado el molesto bug del teleport (falta UI), ajustadas las luces del lobby

--- Assets/Scripts/TeleportActivator.cs
+++ Assets/Scripts/TeleportActivator.cs
@@ -14,6 +14,8 @@ public class TeleportActivator : MonoBehaviour
 
     public XRRayInteractor ray;
 
+    [SerializeField] private XRDirectInteractor rightDirectInteractor;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,8 +29,14 @@ public class TeleportActivator : MonoBehaviour
         //Not working
         //bool isRayHovering = ray.TryGetHitInfo(out Vector3 position, out Vector3 normalPosition, out int number, out bool valid);
         //teleportator.SetActive((!isRayHovering) && (cancelator.action.ReadValue<float>() == 0f && activator.action.ReadValue<float>() > 0.1f));    
+        //teleportator.SetActive(cancelator.action.ReadValue<float>() == 0f && activator.action.ReadValue<float>() > 0.1f);    
         //Debug.Log("RayHover = "  + isRayHovering + " | " + (activator.action.ReadValue<float>() > 0.1f));
-        teleportator.SetActive(activator.action.ReadValue<float>() > 0.1f);
+        //teleportator.SetActive(activator.action.ReadValue<float>() > 0.1f);
+
+
+
+        //Partial solution -> Working for objects in hand, but not with UI interaction.
+        teleportator.SetActive(rightDirectInteractor.interactablesSelected.Count == 0 && activator.action.ReadValue<float>() > 0.1f);
 
     }
 }
