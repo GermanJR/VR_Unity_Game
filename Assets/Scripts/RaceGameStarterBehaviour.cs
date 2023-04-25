0236ceb9b460eb98a9134d3c0fbdff82465b526d
GermanJR <wambows@gmail.com>
2023-04-25 13:17:54 +0200

    Carrera: Funcionalidad de la starting zone -> Funcionando el canvas

--- Assets/Scripts/RaceGameStarterBehaviour.cs
+++ Assets/Scripts/RaceGameStarterBehaviour.cs
@@ -1,21 +1,100 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
 public class RaceGameStarterBehaviour : MonoBehaviour
 {
     public static bool isPlayer1Ready = false;
     public static bool isPlayer2Ready = false;
 
+    [SerializeField] private GameObject playerStartSeparator;
+    [SerializeField] private GameObject textObject;
+
+    [SerializeField] private Animator startBarrierAnimator;
+
+    [SerializeField] private List<GameObject> player1NotReadyTexts;
+    [SerializeField] private List<GameObject> player1ReadyTexts;
+
+    [SerializeField] private List<GameObject> player2NotReadyTexts;
+    [SerializeField] private List<GameObject> player2ReadyTexts;
+
+    private TMP_Text textPlayerStart;
+
+    private PhotonView photonView;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        textPlayerStart = textObject.GetComponent<TMP_Text>();
+        textPlayerStart.text = "";
+
+        photonView = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPlayer1Ready && isPlayer2Ready)
+        {
+            Debug.Log("Race is about to start.");
+            StartCoroutine(StartRaceCoroutine());
+        }
+    }
+
+    IEnumerator StartRaceCoroutine()
+    {
+        playerStartSeparator.SetActive(true);
+        textPlayerStart.text = "READY?";
+        yield return new WaitForSeconds(2f);
+        textPlayerStart.text = "GO!";
+        startBarrierAnimator.SetTrigger("Open");
+        yield return new WaitForSeconds(1f);
+        textPlayerStart.text = "";
+    }
+
+    public void ChangeStatesForPlayer1()
+    {
+        if (player1NotReadyTexts.Count != player1ReadyTexts.Count)
+        {
+            Debug.LogError("Text lists have different capacities");
+            return;
+        }
+
+        photonView.RPC("UpdateStateOverNetworkP1", RpcTarget.All);
+    }
+  
+    public void ChangeStatesForPlayer2()
+    {
+        if (player2NotReadyTexts.Count != player2ReadyTexts.Count)
+        {
+            Debug.LogError("Text lists have different capacities");
+            return;
+        }
+
+        photonView.RPC("UpdateStateOverNetworkP2", RpcTarget.All);
+    }
+
+    [PunRPC]
+    private void UpdateStateOverNetworkP1()
+    {
+        for (int i = 0; i < player1NotReadyTexts.Count; i++)
+        {
+            player1NotReadyTexts[i].SetActive(!player1NotReadyTexts[i].activeSelf);
+            player1ReadyTexts[i].SetActive(!player1ReadyTexts[i].activeSelf);
+        }
+    }
+
+    [PunRPC]
+    private void UpdateStateOverNetworkP2()
+    {
+        for (int i = 0; i < player2NotReadyTexts.Count; i++)
+        {
+            player2NotReadyTexts[i].SetActive(!player2NotReadyTexts[i].activeSelf);
+            player2ReadyTexts[i].SetActive(!player2ReadyTexts[i].activeSelf);
+        }
     }
 }
--- Assets/Scripts/RacePlayer1StartTrigger.cs
+++ Assets/Scripts/RacePlayer1StartTrigger.cs
@@ -1,14 +1,25 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RacePlayer1StartTrigger : MonoBehaviour
 {
+    /*
+    [SerializeField] private List<GameObject> player1NotReadyTexts;
+    [SerializeField] private List<GameObject> player1ReadyTexts;
+    */
+    [SerializeField] private GameObject canvas;
+
+    [SerializeField] private RaceGameStarterBehaviour raceGameStarterBehaviour;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             RaceGameStarterBehaviour.isPlayer1Ready = true;
+            raceGameStarterBehaviour.ChangeStatesForPlayer1();
+            //SwitchTexts();
             Debug.Log("Player1 is ready");
         }
     }
@@ -18,7 +29,32 @@ public class RacePlayer1StartTrigger : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             RaceGameStarterBehaviour.isPlayer1Ready = false;
+            raceGameStarterBehaviour.ChangeStatesForPlayer1();
+            //SwitchTexts();
             Debug.Log("Player1 exited zone");
         }
     }
+
+    /*
+    private void SwitchTexts()
+    {
+        if(player1NotReadyTexts.Count != player1ReadyTexts.Count)
+        {
+            Debug.LogError("Text lists have different capacities");
+            return;
+        }
+
+        canvas.GetComponent<PhotonView>().RPC("UpdateStateOverNetwork", RpcTarget.All);
+    }
+
+    [PunRPC]
+    private void UpdateStateOverNetwork()
+    {
+        for (int i = 0; i < player1NotReadyTexts.Count; i++)
+        {
+            player1NotReadyTexts[i].SetActive(!player1NotReadyTexts[i].activeSelf);
+            player1ReadyTexts[i].SetActive(!player1ReadyTexts[i].activeSelf);
+        }
+    }
+    */
 }
